50f8719b31711b3a82483b53ab0595f5b2c7c6bb
Allen Do <ad@Allens-MacBook-Pro.local>
2020-02-19 03:26:30 -0800

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const AddedFeature = props => {
+  console.log("Added Feature", props.feature)
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick = {e => {e.preventDefault(); props.removeItem(props.feature)}} >X</button>
       {props.feature.name}
     </li>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers/reducers";
+import logger from "redux-logger";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store= createStore(reducer, applyMiddleware(logger))
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
